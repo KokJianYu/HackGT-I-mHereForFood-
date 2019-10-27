36fe22ae4dc45f7ddc8f05827aff5a69fc5b328e
Kok Jian Yu <kokjianyu@gmail.com>
2019-10-26 20:28:30 -0400

    Add ssl key and cert

--- server.py
+++ server.py
@@ -31,12 +31,6 @@ class AsyncGitTask(threading.Thread):
 
       f.close()
 
-class AsyncTimerTask(threading.Thread):
-   def run(self):
-      import time
-      now = time.time()
-
-
 app = Flask(__name__)
 
 
@@ -91,16 +85,12 @@ def audio():
 def index():
     return render_template('index.html')
 
-@app.route('/send_music/<name>')
-def get_recording(name):
-    return flask.send_file(name)
+@app.route('/testtest.wav')
+def get_recording():
+    return flask.send_file("./test.wav")
 
 @app.route('/ui')
 def ui():
-    if request.url.startswith('http://'):
-        url = request.url.replace('http://', 'https://', 1).replace("5000", "5001", 1)
-        code = 301
-        return flask.redirect(url, code=code)
     return render_template('ui.html')
 
 @app.route('/start')
@@ -134,31 +124,13 @@ def send():
 @app.route('/send_text', methods=['POST'])
 def send_text():
     print("received text")
-    mytext = request.form.get('text')
-    mytime = request.form.get('time')
+    data = request.data
+    print(data)
 
-    from gtts import gTTS  
-    import time
 
-    myobj = gTTS(text=mytext, lang='en', slow=False)
-    filename = mytime + ".mp3"
-    myobj.save(filename)
-
-    import sched, time, datetime
-
-    def play_reminder():
-      import requests
-      xml = "<play_info><app_key>CMwhZOwJsgUUclRmJ7k8dpv2KF2F8Qgr</app_key><url>http://192.168.1.85:5000/send_music/" + filename + "</url><service>service text</service><reason>reason text</reason><message>message text</message><volume>50</volume></play_info>"
-      headers = {'Content-Type': 'application/xml'} # set what your server accepts
-      requests.post('http://192.168.1.251:8090/speaker', data=xml, headers=headers)
-
-    # Set up scheduler
-    s = sched.scheduler(time.time, time.sleep)
-    # # Schedule when you want the action to occur
-    s.enterabs(float(mytime)//1000, 0, play_reminder)
-    # # Block until the action has been run
-    s.run()
-
+    # f = open('test.wav', 'wb')
+    # f.write(data)
+    # f.close()
     return "okokokok"
 
 @app.route('/recording', methods=['POST'])
@@ -184,10 +156,10 @@ def recording():
 
 
 def http_app():
-    app.run(host='0.0.0.0', debug=True, threaded=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, threaded=True, port=5001)
 
 if __name__ == "__main__":
     from multiprocessing import Process
 
-    Process(target=http_app, daemon=True).start()
-    app.run(host='0.0.0.0', debug=True, threaded=True, port=5001, ssl_context="adhoc")
+    Process(target=http_app,daemon=True).start()
+    app.run(host='0.0.0.0', debug=True, threaded=True, port=5001, ssl_context=("ssl/domain.crt", "ssl/domain.key"))
